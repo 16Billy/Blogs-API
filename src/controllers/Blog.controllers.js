8e517cd7ab0bd9d0b427dd20b1a8e570a27a7fcf
Billel M <billel.mahmouche@gmail.com>
2025-10-14 06:46:12 -1200

    fixed a small mistake with Blog controllers

--- src/controllers/Blog.controllers.js
+++ src/controllers/Blog.controllers.js
@@ -3,8 +3,8 @@ import { db } from "../db/db.js"
 export const getBlogs = async (req,res) => {
     const author_id = req.author_id
     try {
-        const blogs = db.query('SELECT * from blogs WHERE author_id = $1',[author_id])
-    res.status(200).json(blogs)
+        const blogs = await db.query('SELECT * from blogs WHERE author_id = $1',[author_id])
+        res.status(200).json(blogs)
     } catch (e) {
         res.status(400).json({error:e})
     }
@@ -14,11 +14,11 @@ export const getBlog = async (req, res) => {
     
 }
 
-export const createBlog = (req, res) => {
+export const createBlog = async (req, res) => {
     const {title, body} = req.body
     const author_id = req.author_id
     try {
-        const blog = db.query(`
+        const blog = await db.query(`
         INSERT INTO blogs(title,body,author_id)
         VALUES ($1,$2,$3)
         `, [title,body,author_id])
@@ -30,10 +30,10 @@ export const createBlog = (req, res) => {
 
 }
 
-export const editBlog = (req, res) => {
+export const editBlog = async (req, res) => {
 
 }
 
-export const deleteBlog = (req, res) => {
+export const deleteBlog = async (req, res) => {
 
 }
